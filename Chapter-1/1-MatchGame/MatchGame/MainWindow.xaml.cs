d63705a16472d092aa5ff724d0ed6677e5201cad
Ronaldo Gustavo Ademar <ronaldo.ademar@proponto.com.br>
2025-03-21 22:00:57 -0300

    Inclusão de lista de emojis e sortimento. Atribuição randomização dos emojis

--- Chapter-1/1-MatchGame/MatchGame/MainWindow.xaml.cs
+++ Chapter-1/1-MatchGame/MatchGame/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,38 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+            SetupGame();
+        }
+
+        private void SetupGame()
+        {
+            List<string> animalsEmoji = new List<string>()
+           {
+               "🐶", "🐶",
+               "🐱", "🐱",
+               "🐭", "🐭",
+               "🐹", "🐹",
+               "🐰", "🐰",
+               "🦊", "🦊",
+               "🐻", "🐻",
+               "🐼", "🐼"
+           };
+
+            Random sortAnimals = new Random();
+            var textBlocks = mainGrid.Children.OfType<TextBlock>().ToList();
+            Debug.WriteLine($"Número de TextBlocks: {textBlocks.Count}");
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                // Check if there are still emojis available
+                if (animalsEmoji.Count == 0)
+                    break;  // or regenerate the list if you need more emojis
+
+                int index = sortAnimals.Next(animalsEmoji.Count);
+                string nextEmoji = animalsEmoji[index];
+                textBlock.Text = nextEmoji;
+                textBlock.Tag = nextEmoji;
+                animalsEmoji.RemoveAt(index);
+            }
         }
     }
 }
